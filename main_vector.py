497b039a22c4dff752bde673b3d6473cf7e913ed
charlieDJ <dj278016981@gmail.com>
2021-04-01 18:08:37 +0800

    添加一些内置方法

---
+++ main_vector.py
@@ -0,0 +1,7 @@
+from playLA.Vector import Vector
+
+if __name__ == '__main__':
+    u = Vector([5, 2])
+    print(u)
+    print(u.__getitem__(1))
+    print(u.__len__())
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -2,6 +2,12 @@ class Vector:
     def __init__(self, lst):
         self._values = lst
 
+    def __len__(self):
+        return len(self._values)
+
+    def __getitem__(self, index):
+        return self._values[index]
+
     #  系统调用
     def __repr__(self):
         return "Vector({})".format(self._values)
