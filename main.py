22359faff06d66dd25750c446e1027caa9e6a434
charlieDJ <dj278016981@gmail.com>
2021-04-01 18:01:02 +0800

    修改main方法

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from playLA.Vector import Vector
 
 
 def print_hi(name):
@@ -11,6 +12,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    u = Vector([5, 2])
+    print(u)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
